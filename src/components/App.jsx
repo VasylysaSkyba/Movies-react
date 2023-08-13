688a1adf039578903d7bd70ddee1eb610555ab87
Vasylysaskyba <vasylysaskyba@gmail.com>
2023-08-14 00:10:51 +0300

    Маршрутизація

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,24 @@
+import { Route } from 'react-router-dom';
 import Navigation from './Navigation/Navigation';
 
 const App = () => {
-    return (
-        <Navigation />
-    )
-}
+  return (
+    <Container>
+      <Navigation />
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/movies" element={<MoviesPage />} />
+          <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
+            <Route path="cast" element={<CastPage />} />
+            <Route path="reviews" element={<ReviewsPage />} />
+          </Route>
+
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Suspense>
+    </Container>
+  );
+};
 
 export default App;
