c4fc3d53fa65f624082a21f8a76654ee21224335
Vasylysaskyba <vasylysaskyba@gmail.com>
2023-08-14 01:28:20 +0300

    homepage, useEffect, services, fetch

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,4 +1,37 @@
-const HomePage = () => { };
+import styles from './HomePage.module.css';
+import { useEffect, useState } from 'react';
+import { fetchTrendingMovies } from 'services/api-movies';
+import Loader from 'components/Loader/Loader';
+import MoviesEditorList from 'components/MoviesEditorList/MoviesEditorList';
 
+const HomePage = () => {
+  const [movies, setMovies] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-export default HomePage
+  useEffect(() => {
+    const fetchTrending = async () => {
+      setLoading(true);
+      try {
+        const { results } = await fetchTrendingMovies();
+        setMovies(results);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchTrending();
+  }, []);
+
+  return (
+    <main>
+      {loading && <Loader />}
+      <h1 className={styles.title}>Tranding today</h1>
+      {movies && <MoviesEditorList movies={movies} />}
+      {error && <p>Something goes wrong</p>}
+    </main>
+  );
+};
+
+export default HomePage;
--- src/services/api-movies.js
+++ src/services/api-movies.js
@@ -7,3 +7,36 @@ const instance = axios.create({
         language: 'en-Us',
     },
 });
+
+export const IMAGE_URL = 'https://image.tmdb.org/t/p/w500';
+
+export const fetchTrendingMovies = async () => {
+  const { data } = await instance.get('/trending/movie/day');
+  return data;
+};
+
+export const searchMovies = async query => {
+  const { data } = await instance.get('search/movie', {
+    params: {
+      page: 1,
+      include_adult: false,
+      query,
+    },
+  });
+  return data;
+};
+
+export const fetchMovieDetails = async id => {
+  const { data } = await instance.get(`movie/${id}`);
+  return data;
+};
+
+export const fetchMovieActors = async id => {
+  const { data } = await instance.get(`movie/${id}/credits`);
+  return data;
+};
+
+export const fetchMovieReviews = async id => {
+  const { data } = await instance.get(`movie/${id}/reviews`);
+  return data;
+};
